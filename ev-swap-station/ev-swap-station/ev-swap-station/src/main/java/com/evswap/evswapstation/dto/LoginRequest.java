88ec5f0278a9342776e35e564508fe82a1d04c2e
Thân Lê Thành Vinh <thanhvinh832004@gmail.com>
2025-10-06 14:53:08 +0700

    fix lại file chính (file kia lỗi)

--- ev-swap-station/ev-swap-station/ev-swap-station/src/main/java/com/evswap/evswapstation/dto/LoginRequest.java
+++ ev-swap-station/ev-swap-station/ev-swap-station/src/main/java/com/evswap/evswapstation/dto/LoginRequest.java
@@ -1,13 +1,9 @@
 package com.evswap.evswapstation.dto;
 
-import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
-//public class LoginRequest {
-//    private String userName;
-//    private String password;
-//}
-
-public class LoginRequest { @NotBlank
-private String username; @NotBlank private String password; }
+public class LoginRequest {
+    private String userName;
+    private String password;
+}
--- ev-swap-station/ev-swap-station/ev-swap-station/src/main/java/com/evswap/evswapstation/dto/RegisterRequest.java
+++ ev-swap-station/ev-swap-station/ev-swap-station/src/main/java/com/evswap/evswapstation/dto/RegisterRequest.java
@@ -1,28 +1,14 @@
 package com.evswap.evswapstation.dto;
 
-import com.evswap.evswapstation.entity.Role;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
-//public class RegisterRequest {
-//    private String userName;
-//    private String password;
-//    private String fullName;
-//    private String phone;
-//    private String email;
-//    private String address;
-//}
-
 public class RegisterRequest {
-    @NotBlank
-    private String username;
-    @NotBlank private String password;
-    @NotNull  private Role role; // Driver/Staff/Admin
-    @NotBlank private String fullName;
-    @NotBlank private String phone;
-    @NotBlank private String email;
-    @NotBlank private String address;
+    private String userName;
+    private String password;
+    private String fullName;
+    private String phone;
+    private String email;
+    private String address;
 }
 
